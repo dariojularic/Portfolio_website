af347c4e2a73efde66c98ada1eda3abb93ef02d8
Dario Jularic <dariojularic0@gmail.com>
2025-08-24 11:26:55 +0200

    Mount app

---
+++ public/main.ts
@@ -0,0 +1,17 @@
+async function fetchMessage(): Promise<void> {
+  try {
+    const response = await fetch("http://localhost:8080");
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    console.log("Message from server:", data.message);
+  } catch (error) {
+    console.error("There was a problem with the fetch operation:", error);
+  }
+}
+
+fetchMessage()
