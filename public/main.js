7bb45eada5a88bbd14ac5915b36cbed6df6abbef
Dario Jularic <dariojularic0@gmail.com>
2025-09-03 16:03:54 +0200

    Create event listeners

--- public/main.js
+++ public/main.js
@@ -1,5 +1,32 @@
-const aboutNavBtn = document.querySelector(".about-nav-btn")
-const skillsNavBtn = document.querySelector(".skills-nav-btn")
-const portfolioNavBtn = document.querySelector(".portfolio-nav-btn")
-const contactNavBtn = document.querySelector(".contact-nav-btn")
+const aboutNavBtn = document.querySelector(".about-nav-btn");
+const skillsNavBtn = document.querySelector(".skills-nav-btn");
+const portfolioNavBtn = document.querySelector(".portfolio-nav-btn");
+const contactNavBtn = document.querySelector(".contact-nav-btn");
+const aboutSection = document.querySelector(".about-section");
+const skillsSection = document.querySelector(".skills-section");
+const portfolioSection = document.querySelector(".portfolio-section");
+const contactSection = document.querySelector(".contact-section");
 
+aboutNavBtn.addEventListener("click", () => {
+  aboutSection.scrollIntoView({
+    behavior: "smooth",
+  });
+});
+
+skillsNavBtn.addEventListener("click", () => {
+  skillsSection.scrollIntoView({
+    behavior: "smooth",
+  });
+});
+
+portfolioNavBtn.addEventListener("click", () => {
+  portfolioSection.scrollIntoView({
+    behavior: "smooth",
+  });
+});
+
+contactNavBtn.addEventListener("click", () => {
+  contactSection.scrollIntoView({
+    behavior: "smooth",
+  });
+});
