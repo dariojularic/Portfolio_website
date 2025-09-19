a6f5e2339d5c50a1d67de4a8a92b45835bb03f8a
Dario Jularic <dariojularic0@gmail.com>
2025-09-19 10:02:34 +0200

    Add style

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-// pojasnit ovaj process.env.PORT
 const port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, "public")));
@@ -16,13 +15,3 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port);
-
-// css responsivness
-// css animacija on scroll
-// pogledat intersectionObserver api - prvo neka radi na jednu sekciju
-//
-//
-
-
-// scrolanje projekata
-// ocu u verziji za mobitele stavit onaj hamburger button ili mozda bottom navi koja je stalno na dnu ekrana??
