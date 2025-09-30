c4ab62f616adc2636c442806427aae5266b44ce8
Dario Jularic <dariojularic0@gmail.com>
2025-09-30 11:45:16 +0200

    Refactor code

--- public/main.js
+++ public/main.js
@@ -42,7 +42,7 @@ sections.forEach((section) => observer.observe(section));
 const splide = new Splide(".splide", {
   type: "loop",
   perPage: 3,
-  autoplay: true,
+  autoplay: false,
   interval: 3000,
   gap: 100,
   pauseOnHover: true,
